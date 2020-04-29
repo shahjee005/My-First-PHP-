d5b95e70955888783b4aa846516fae79b84ab287
Syed-Gardezi <61136718+shahjee005@users.noreply.github.com>
2020-04-29 11:16:34 -0600

    added another file datatypes.php and shifted all the varibales into that file

---
+++ includes/datatypes.php
@@ -0,0 +1,42 @@
+<?php 
+$myMessege= 'My First PHP';
+
+//Data types 
+//String 
+$myString= 'hellow , World!';
+// integer 
+$myInteger= 50;
+//Float 
+$myFloat=20.02;
+//Boolean
+$myBoolTrue=TRUE;
+$myboolFalse = false;
+//Null 
+$myNull= NULL;
+//Object
+$myobject= new stdClass();
+//Array 
+$myArray =['First item ', 2, 'third item'];
+$myOtherArray= array (
+    $myString,
+    $myInteger,
+    $myobject,
+    $myFloat,
+    $myNull,
+    $myboolFalse,
+    $myBoolTrue,
+);
+
+//Strings 
+$string1= 'hello , my name is ';
+$string2='Michel jackson';
+// Strings can be concatenated by ising "." ("+" is reserved for addition )
+$concattedString= $string1.$string2;
+// Difference between single and double quotes.
+$mySingleQuoteHelloString = 'Hi there! My name is $string2';
+// Double quotes are PARSED. PHP checks for variable names to echo out.
+$myDoubleQuoteHelloString = "Howdy there! My name is $string2";
+//This is more specific , and would be good if you have overlaping variable names.
+$myDoubleQuoteHelloString2= " Howday there! My name is {$string2}"; 
+
+?>
---
+++ includes/navigation.php
@@ -0,0 +1,13 @@
+<nav> 
+<h1>Webiste Navigation  </h1>
+<ul>  
+<li> 
+<a href ="./index.php"> Home </a>
+</li>
+</ul>
+ </nav> 
+ <?php // By deafult, antthing written directly in a PHP file wil be sent 
+ // to the browser ( or whatever client made the request.)
+ // see above how i have some HTML outputting, as I did not inculde any PHP tags 
+ //(<?php etc.) that would tell PHP otherwise.
+ 
